e9539677a044b113128513708158f95f17fe2b02
Oscar Ramirez <osramirez@csumb.edu>
2018-09-13 18:09:51 +0000

    Added Whole Class

---
+++ Homework/HW1-Fall18-CST336/how.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset = "utf-8" />
+        <title>Instructions</title>
+        <link href = "css/stylesheet.css" rel = "stylesheet" type = "text/css" />
+    </head>
+    
+    <body style = "background-color: #003434; color: white;">
+        <!-- #4C636B -->
+        <header>
+            <h1><strong>The Instructions on how the Turing Machine was Utilized</strong></h1>
+             <nav>
+                <a href = "importance.php"><strong>Motive</strong></a>
+                <a href = "index.php" ><strong>Home</strong></a>
+                <a href = "person.php"><strong>Alan</strong></a>
+            </nav>
+            <hr>
+        </header>
+        
+        <main>
+            <section>
+                <h3 id = "component">Here is how they operated instructions on the Turing Machine.</h3>
+                <img id = "instructions" src = "img/Instructions.JPG" width = "500" height = "400" alt = "instructions" />
+            </section>
+        </main>
+        <footer>
+            <hr>
+                Internet Programming CST336 2018 &copy; Ramirez <br />
+                <strong> Disclaimer: </strong> The information in this webiste is fictitous. <br />
+                It is for academics purposes only. <br />
+                <img width = "100" height = "50" src = "img/CSUMB_Logo.png" alt = "logo" />
+        </footer>
+    </body>
+    
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Class CST336</title>
+        
+    </head>
+    <bodyy>
+        <header>
+            <h1>CST 336 Fall 2018</h1>
+            <a href = "Homework"><strong>Homework</strong></a>
+            <br >
+            <a href = "Labs"><strong>Labs</strong></a>
+            
+        </header>
+    </bodyy>
+</html>
