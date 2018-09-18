cf6972b344823b852faeb6389093a421cf350470
Oscar Ramirez <osramirez@csumb.edu>
2018-09-18 00:49:53 +0000

    Adding Index Back

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Class CST336</title>
+        
+    </head>
+    <body>
+        <header>
+            <h1>CST 336 Fall 2018</h1>
+            <a href = "Homework"><strong><h2>Homework</h2></strong></a>
+            <br >
+            <a href = "Labs"><strong><h2>Labs</h2></strong></a>
+            
+        </header>
+    </body>
+</html>
