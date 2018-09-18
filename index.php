9b4669102a7f11811bed68ff6098e066d1da5484
Oscar Ramirez <osramirez@csumb.edu>
2018-09-18 04:19:17 +0000

    Optimized Linking

--- index.php
+++ index.php
@@ -1,16 +1,70 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8"/>
         <title>Class CST336</title>
         
+        <style>
+            section{
+                
+                height: 600px;
+            }
+            
+            h2{
+                color: cyan;
+                border-style: double;
+            }
+
+            a{
+                color: red;
+            }
+        </style>
+        
     </head>
-    <body>
+    <body style = "background-color: black; color: white" >
+        
         <header>
-            <h1>CST 336 Fall 2018</h1>
-            <a href = "./Homework/HW 1 - Alan Turing"><strong><h2>Homework 1</h2></strong></a>
-            <br >
-            <a href = "./Labs/Lab 1 - Portfolio"><strong><h2>Labs</h2></strong></a>
+            <h1 style = "text-align: center">CST 336 Fall 2018</h1>
             
         </header>
+        <hr style = "width: 50%">
+        
+        <main>
+            
+            <section style = "width: 49%; float: left">
+                
+                <div style = "text-align: center;">
+                    
+                    <h2><strong>Homework</strong></h2>
+                    
+                    <a href = "./Homework/HW 1 - Alan Turing"><strong><h3>Homework 1 - Alan Turing</h3></strong></a>
+            
+                </div>
+                
+            </section>
+            
+            <section style = "width: 49%; float: right">
+                
+                <div style = "text-align: center;">
+                    
+                    <h2><strong>Labs</strong></h2>
+                    
+                    <a href = "./Labs/Lab 1 - Portfolio"><strong><h3>Lab 1 - Portfolio</h3></strong></a>
+                    <a href = "./Labs/Lab 2 - Slot Machine"><strong><h3>Lab 2 - Slot Machine</h3></strong></a>
+                    
+                </div>
+            
+            </section>
+            
+        </main>
+        
+        <footer style = "text-align: center">
+            <hr>
+                Internet Programming CST336 2018 &copy; Ramirez <br />
+                <strong> Disclaimer: </strong> The information in this webiste is fictitous. <br />
+                It is for academics purposes only. <br />
+                <img width = "100" height = "50" src = "CSUMB_Logo.png" alt = "logo" />
+        </footer>
+        
     </body>
 </html>
