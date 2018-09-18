a63f4d9f13905ed77495dd01f9afdb8dfbc893ec
Oscar Ramirez <osramirez@csumb.edu>
2018-09-18 01:20:25 +0000

    Finalization

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
     <body>
         <header>
             <h1>CST 336 Fall 2018</h1>
-            <a href = "Homework"><strong><h2>Homework</h2></strong></a>
+            <a href = "./Homework/HW 1 - Alan Turing"><strong><h2>Homework 1</h2></strong></a>
             <br >
-            <a href = "Labs"><strong><h2>Labs</h2></strong></a>
+            <a href = "./Labs/Lab 1 - Portfolio"><strong><h2>Labs</h2></strong></a>
             
         </header>
     </body>
