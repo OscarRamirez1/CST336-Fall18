20030cfe29207e2511a67e8fb3e2a9559db6fe67
Oscar Ramirez <osramirez@csumb.edu>
2018-10-03 20:41:57 +0000

    Updated

--- Labs/Lab 4 - Carousel/index.php
+++ Labs/Lab 4 - Carousel/index.php
@@ -1,95 +1,108 @@
 <?php
     $backgroundImage = "img/sea.jpg";
     
-    if(isset($_GET['keyword']))
-    {
+    if (isset($_GET['keyword'])) {
         include 'api/pixabayAPI.php';
-        $imageURLs = getImageURLs($_GET['keyword']);
+        $keyword = $_GET['keyword'];
+        $imageURLs = getImageURLs($keyword);
         $backgroundImage = $imageURLs[array_rand($imageURLs)];
-        echo "Your searched for: " .$_GET['keyword'];
     }
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Image Carousel</title>
+        <meta charset="utf-8">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
         <style>
             @import url("css/styles.css");
-            body
-            {
+            body {
                 background-image: url('<?=$backgroundImage ?>');
             }
         </style>
     </head>
+    
     <body>
-        <br/> <br/>
+        <br>
         <?php
-            if(!isset($imageURLs)) 
-            {
-                echo "<h2> Type a keyword to display a slideshow <br/> with random images from Pixabay.com </h2>";
-            }
-            else 
-            {
+            if (!isset($imageURLs)) {
+                echo "<h2> Type a keyword to display a slideshow with random images from Pixabay.com </h2>";
+            } else {
+                
         ?>
+            
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-        <?php 
-            for($i = 0; $i < 7; $i++)
-            {
-                echo "<li data-target='#carousel-example-generic' data-slide-to='$i'";
-                echo ($i == 0)?" class='active'": "";
-                echo "></li>";
-            }
-        ?>
-        </ol>
-        <div class="carousel-inner" role="listbox">
-        <?php
-            for($i = 0; $i < 7; $i++)
-            {
-                do {
-                        $randomIndex = rand(0,count($imageURLs));
-                    } while(!isset($imageURLs[$randomIndex]));
-                echo '<div class="item ';
-                echo ($i == 0)?"active": "";
-                echo '">';
-                echo "<img src='" . $imageURLs[$randomIndex] . "' width='200' >";
-                echo '</div>';
-                unset($imageURLs[$randomIndex]);
-            }
-        ?>
+                
+            <ol class="carousel-indicators">
+                <?php
+                    for($i = 0; $i < 7; $i++) {
+                        echo "<li data-target='#carousel-example-generic' data-slide-to='$i'";
+                        echo ($i == 0) ? "class = 'active'" : "";
+                        echo "></li>";
+                    }
+                ?>    
+            </ol>
         </div>
+            
+        <div class="carousel-inner" role="listbox">
+            <?php
+               for($i = 0; $i < 7; $i++) {
+                   do {
+                    $randomIndex = rand(0,count($imageURLs));
+                    } while(!isset($imageURLs[$randomIndex]));
+                    
+                    echo '<div class="carousel-item ';
+                    echo ($i == 0) ? "active" : "";
+                    echo '">';
+                    echo '<img src="' . $imageURLs[$randomIndex] . '">';
+                    echo '</div>';
+                    unset($imageURLs[$randomIndex]);
+                }
+            ?>
+        </div>
+            
         <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
+        
         <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
             <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
-        </div> 
+        
+        </div>
+        
         <?php
             }
         ?>
-        <h1> I'm a:  </h1>
+        
+        <br>
+        
         <form>
-            <input type = "radio" id = "lhorizontal" name = "layout" value = "horizontal" />
-            <label for = "Horizontal"></label><label for = "lhorizontal">Horizontal</label>
-            <input type = "radio" id = "lvertical" name = "layout" value = "verical"/>
-            <label for = "Verical"></label><label for = "lvertical">Vertical</label>
-            <select name="keyword">
-              <option value="none">Select One</option>
-              <option value="forest">Forest</option>
-              <option value="sky">Sky</option>
-              <option value="otters">Otters</option>
-              <option value="cars">Cars</option>
-            </select> <br/><br/>
+            <input type="text" name="keyword" placeholder="keyword" value="<?=$_GET['keyword']?>"/>
             
-            <input type="submit" value="Submit"/>
-            <input type="text" name="keyword" placeholder="Keyword" value = "<?= $_GET['keyword']?>"/>
+            <input type="radio" id="lhorizontal" name="layout" value="horizontal">
+            <label for = "Horizontal"></label><label for ="lhorizontal">Horizontal</label>
+            
+            <input type="radio" id="lvertical" name="layout" value="vertical">
+            <label for="Vertical"></label><label for="lvertical">Vertical</label>
+            
+            <select name="category">
+                <option value = "">Select One</option>
+                <option value = "ocean">Sea</option>
+                <option>Forest</option>
+                <option>Mountain</option>
+                <option>Snow</option>
+            </select>
+            
+            <input type="submit" value="Search" />
         </form>
-        <br/> <br/>
+        <br/> <br />
+        
+        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity = sha384></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     </body>
 </html>
